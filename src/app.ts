458e888b84da7c803cc18841e45581ecfcadce1f
cascateer <cascateer@gmail.com>
2026-05-24 22:04:46 +0200

    form data

--- src/app.ts
+++ src/app.ts
@@ -176,12 +176,12 @@ app.get("/spotify/auth-callback", async (req, res) =>
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
         },
-        body: {
-          client_id: process.env.SPOTIFY_CLIENT_ID,
+        body: new URLSearchParams({
+          client_id: process.env.SPOTIFY_CLIENT_ID!,
           grant_type: "authorization_code",
-          code: req.query.code,
-          redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
-        },
+          code: String(req.query.code),
+          redirect_uri: process.env.SPOTIFY_REDIRECT_URI!,
+        }),
       })
       .then(({ data }) => data),
   ),
