2b4e80c44bf87daa5cde23691566a18a5a3914b8
cascateer <cascateer@gmail.com>
2026-05-24 18:54:18 +0200

    rollback

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,7 @@ const oauth2Client = new google.auth.OAuth2(
 app.use(json());
 app.use(
   cors({
-    origin: true,
+    origin: ["http://localhost:5173", "https://cascateer.dev"],
     credentials: true,
   }),
 );
@@ -39,6 +39,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     secret: "secret secret",
+    store: redisStore,
   }),
 );
 
